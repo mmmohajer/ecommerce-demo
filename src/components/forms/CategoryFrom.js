22bc67ece38b5d3d44cf388e0406c08c6e2d1819
Mohammad Mohajer <55400075+mmmohajer@users.noreply.github.com>
2020-12-08 17:21:30 -0500

    Major changes

---
+++ src/components/forms/CategoryFrom.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const CategoryFrom = ({ handleSubmit, name, setName }) => {
+	return (
+		<form onSubmit = {handleSubmit}>
+			<div className="form-group">
+				<label>Name</label>
+				<input
+					type = "text"
+					className = "form-control"
+					value = { name } 
+					onChange = { (e) => setName(e.target.value) }
+					autoFocus
+					required
+				/>
+				<br />
+				<button className="btn btn-outline-primary">Save</button>
+			</div>
+		</form>
+	)
+}
+
+export default CategoryFrom;
